ecd1652740ce3b5736f3dcbdd82e22dcf02251cf
PorcupineRain <rayne.jurchen@gmail.com>
2024-08-28 12:51:08 +0200

    add e2e testing

--- todo-app-with-cypress/cypress/e2e/todo-app.cy.js
+++ todo-app-with-cypress/cypress/e2e/todo-app.cy.js
@@ -1,12 +1,33 @@
 /// <reference types="cypress"/>
 
 describe("Todo App", () => {
-  beforeEach(() => {
+  beforeEach("check if server is live", () => {
     cy.visit("http://localhost:3000");
   });
 
   it("should have input field", () => {
     cy.get("#toDoInput").should("exist");
+  });
+
+  it("should have filters", () => {
+    cy.get("#filterNone").should("exist");
+    cy.get("#filterDone").should("exist");
+    cy.get("#filterOpen").should("exist");
+  });
+
+  it("should filter none", () => {
+    cy.get("#filterNone").click();
+    cy.get("#list").get("li").should("have.length", 3);
+  });
+
+  it("should filter done", () => {
+    cy.get("#filterDone").click();
+    cy.get("#list").get("li").should("have.length", 2);
+  });
+
+  it("should filter open", () => {
+    cy.get("#filterOpen").click();
+    cy.get("#list").get("li").should("have.length", 1);
   });
 
   it("should add new elements", () => {
@@ -15,9 +36,18 @@ describe("Todo App", () => {
     cy.get("#list").get("li").should("have.length", 4);
   });
 
-  it("should have filters", () => {
-    cy.get("#filterNone").should("exist");
-    cy.get("#filterDone").should("exist");
-    cy.get("#filterOpen").should("exist");
+  it("should delete done", () => {
+    cy.get("#removeDone").click();
+    cy.get("#list").get("li").should("have.length", 1);
+    cy.get("li").find("input[type=checkbox]").should("not.be.checked");
+  });
+
+  it("should not allow duplicates", () => {
+    cy.get("#toDoInput").type("learn JS");
+    cy.get("#addButton").click();
+    cy.get("#list").get("li").should("have.length", 4);
+    cy.get("#toDoInput").type("learn JS");
+    cy.get("#addButton").click();
+    cy.get("#list").get("li").should("have.length", 4);
   });
 });
